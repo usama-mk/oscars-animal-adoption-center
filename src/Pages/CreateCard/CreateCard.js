d860a4e19c90ab05f48c8801d80470eb007ce45d
Usama Muhammad <usamamuhammad45678@gmail.com>
2021-10-13 04:33:36 +0500

    create animal post

--- src/Pages/CreateCard/CreateCard.js
+++ src/Pages/CreateCard/CreateCard.js
@@ -1,11 +1,136 @@
-import React from 'react'
-import './CreateCard.css'
+import React, { useState } from "react";
+import "./CreateCard.css";
+import { MDBInput } from "mdbreact";
+import { Button } from "react-bootstrap";
+
 function CreateCard() {
-    return (
-        <div className="CreateCard">
-            
-        </div>
-    )
+  // const[name, setName]= useState('')
+  const [name, setName] = useState("");
+  const [specie, setSpecie] = useState("");
+  const [breed, setBreed] = useState("");
+  const [color, setColor] = useState("");
+  const [description, setDescription] = useState("");
+  const [month, setMonth] = useState();
+  const [year, setYear] = useState();
+  const [type, setType] = useState("");
+  const [age, setAge] = useState("");
+  const [notes, setNotes] = useState("");
+  const [status, setStatus] = useState("");
+  const [before, setBefore] = useState("");
+
+var posts=[]
+
+  const handleCreateCard = (e) => {
+     
+    e.preventDefault();
+
+    posts.push({
+        name: name,
+        breed: breed
+    })
+
+    console.log(posts)
+    
+  };
+
+  const handleSelectChange = (option) => {
+    setType(option.value);
+  };
+  return (
+    <div className="CreateCard flex-center">
+      <form onSubmit={handleCreateCard}>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          required
+          placeholder="Name"
+        />
+        <input
+          value={specie}
+          onChange={(e) => setSpecie(e.target.value)}
+          type="text"
+          required
+          placeholder="Specie"
+        />
+        <input
+          value={breed}
+          onChange={(e) => setBreed(e.target.value)}
+          type="text"
+          required
+          placeholder="Breed"
+        />
+        <input
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          type="text"
+          required
+          placeholder="Color"
+        />
+        <br />
+        <br />
+        <select onChange={handleSelectChange} value={type} required name="type">
+          <option value="Jack">Jack</option>
+          <option value="Jenny">Jenny</option>
+          <option value="Gelding">Gelding</option>
+        </select>
+        <br />
+        <br />
+        <MDBInput
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+          required
+          hint="Description.."
+        />
+        <br />
+        <input
+          value={month}
+          onChange={(e) => setMonth(e.target.value)}
+          required
+          type="text"
+          placeholder="Month"
+        />
+        <input
+          value={year}
+          onChange={(e) => setYear(e.target.value)}
+          required
+          type="number"
+          placeholder="Year"
+        />
+        <input
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          required
+          type="number"
+          placeholder="Age"
+        />
+        <br />
+        <br />
+        <MDBInput
+          value={notes}
+          onChange={(e) => setNotes(e.target.value)}
+          required
+          hint="Intake Notes"
+        />
+        <MDBInput
+          value={status}
+          onChange={(e) => setStatus(e.target.value)}
+          required
+          hint="Status"
+        />
+        <MDBInput
+          value={before}
+          onChange={(e) => setBefore(e.target.value)}
+          required
+          hint="Before Oscar's Place"
+        />
+        <Button required type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
 }
 
-export default CreateCard
+export default CreateCard;
