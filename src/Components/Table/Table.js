b9661b882cf12c1fee28a64588253c83ebfdad8a
Usama Muhammad <usamamuhammad45678@gmail.com>
2021-10-16 03:01:54 +0500

    table firebase data

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -1,14 +1,86 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import "./Table.css";
 import * as ReactBootStrap from "react-bootstrap";
+import { collection, doc, onSnapshot, getDoc, getDocs, setDoc, query, where } from "@firebase/firestore";
+import { db } from "../../firebase";
 
-function Table() {
-  const [comments, setComments] = useState(["one", "two", "three", ]);
+function Table({ id }) {
+  const [comments, setComments] = useState(["one", "two", "three"]);
+  const [newComment, setNewComment] = useState({
+    date: "",
+    examDetails: "",
+    nextSteps: "",
+  });
   const [date, setDate] = useState("12/1/2020");
   const [examDetails, setExamDetails] = useState("rhino/flu");
   const [nextSteps, setNextSteps] = useState("Cattrina Lucas");
   const [admin, setAdmin] = useState(true);
   const [editor, setEditor] = useState(true);
+
+  const updateComment = async (e) => {
+    const commentsRef = doc(collection(db, "animalsPost", id, "comments"));
+     
+      const newValue = e.target.value;
+      //coll doc coll doc
+      await setDoc(commentsRef, { comment: newComment });
+
+
+    // if (e.target.name == "commentDate") {
+    //   setNewComment({ ...newComment, date: e.target.value });
+    //   const newValue = e.target.value;
+    //   //coll doc coll doc
+    //   await setDoc(commentsRef, { comment: { ...newComment, date: newValue } });
+    // }
+
+    // if (e.target.name == "commentExamDetails") {
+    //   setNewComment({ ...newComment, examDetails: e.target.value });
+    //   const newValue = e.target.value;
+    //   //coll doc coll doc
+    //   await setDoc(commentsRef, {
+    //     comment: { ...newComment, examDetails: newValue },
+    //   });
+    // }
+
+    // if (e.target.name == "commentNextSteps") {
+    //   setNewComment({ ...newComment, nextSteps: e.target.value });
+    //   const newValue = e.target.value;
+    //   //coll doc coll doc
+    //   await setDoc(commentsRef, {
+    //     comment: { ...newComment, nextSteps: newValue },
+    //   });
+    // }
+  };
+
+  useEffect( async()=>{
+  //   const querySnapshot = await getDocs(collection(db, "animalsPost", id, "comments"));
+     
+  //    var tempComments=[]
+
+  // querySnapshot.forEach((doc) => {
+  //   console.log(doc.id, doc.data());
+
+  //   tempComments.push({
+  //   id: doc.id,
+  //   data: doc.data()
+  // })
+  // })
+
+  const q = query(collection(db, "animalsPost", id, "comments"));
+const unsubscribe = onSnapshot(q, (querySnapshot) => {
+  const tempComments = [];
+  querySnapshot.forEach((doc) => {
+    tempComments.push({
+      id: doc.id,
+      data: doc.data()
+    });
+  });
+  // console.log("Current cities in CA: ", tempComments);
+  setComments(tempComments)
+  console.log(tempComments[0].data)
+});
+
+  // setComments(tempComments)
+  },[])
 
   return (
     <div className="Table">
@@ -20,80 +92,111 @@ function Table() {
             <th className="center">Notes/Next Steps</th>
           </tr>
         </thead>
-         <tbody>
+        <tbody>
+          {comments.map((comment) => {
+            return (
+              <tr className="center">
+                <td>
+                  {admin || editor ? (
+                    <input
+                      type="text"
+                      value={ comment.data.comment.date}
+                      
+                      onChange={(e) => setDate(e.target.value)}
+                    />
+                  ) : (
+                    comment.data.comment.date
+                  )}
+                </td>
+                <td>
+                  {admin || editor ? (
+                    <input
+                      type="text"
+
+                      value={ comment.data.comment.examDetails}
+                      onChange={(e) => setExamDetails(e.target.value)}
+                    />
+                  ) : (
+                    comment.data.comment.examDetails
+                  )}
+                </td>
+                <td>
+                  {admin || editor ? (
+                    <input
+                      type="text"
+                      value={ comment.data.comment.nextSteps}
+                      onChange={(e) => {
+                        setNextSteps(e.target.value);
+                      }}
+                    />
+                  ) : (
+                    comment.data.comment.nextSteps
+                  )}
+                </td>
+              </tr>
+            );
+          })}
+          {/* NEW Comment */}
           <tr className="center">
             <td>
               {admin || editor ? (
                 <input
                   type="text"
-                  value={date}
-                  onChange={(e) => setDate(e.target.value)}
+                  name="commentDate"
+                  value={newComment.date}
+                  onChange={(e) => {
+                    setNewComment({
+                      ...newComment,
+                      date: e.target.value,
+                    });
+                  }}
                 />
               ) : (
-                date
+                newComment.date
               )}
+            </td>
+            <td>
+              {admin || editor ? (
+                <Fragment>
+                  <input
+                  type="text"
+                  name="commentExamDetails"
+                  value={newComment.examDetails}
+                  onChange={(e) => {
+                    setNewComment({
+                      ...newComment,
+                      examDetails: e.target.value,
+                    });
+                  }}
+                />
+                <ReactBootStrap.Button onClick={(e)=>{
+                  updateComment(e)
+                }} >
+                  Add Comment
+                </ReactBootStrap.Button>
+                  </Fragment>
+                
+              ) : (
+                newComment.examDetails
+              )
+              }
             </td>
             <td>
               {admin || editor ? (
                 <input
                   type="text"
-                  value={examDetails}
-                  onChange={(e) => setExamDetails(e.target.value)}
+                  name="commentNextSteps"
+                  value={newComment.nextSteps}
+                  onChange={(e) => {
+                    setNewComment({ ...newComment, nextSteps: e.target.value });
+                  }}
                 />
               ) : (
-                examDetails
-              )}
-            </td>
-            <td>
-              {admin || editor ? (
-                <input
-                  type="text"
-                  value={nextSteps}
-                  onChange={(e) => setNextSteps(e.target.value)}
-                />
-              ) : (
-                nextSteps
-              )}
-            </td>
-          </tr>
-           
-            <tr className="center">
-            <td>
-              {admin || editor ? (
-                <input
-                  type="text"
-                  value={date}
-                  onChange={(e) => setDate(e.target.value)}
-                />
-              ) : (
-                date
-              )}
-            </td>
-            <td>
-              {admin || editor ? (
-                <input
-                  type="text"
-                  value={examDetails}
-                  onChange={(e) => setExamDetails(e.target.value)}
-                />
-              ) : (
-                examDetails
-              )}
-            </td>
-            <td>
-              {admin || editor ? (
-                <input
-                  type="text"
-                  value={nextSteps}
-                  onChange={(e) => setNextSteps(e.target.value)}
-                />
-              ) : (
-                nextSteps
+                newComment.nextSteps
               )}
             </td>
           </tr>
         </tbody>
-        
       </ReactBootStrap.Table>
     </div>
   );
