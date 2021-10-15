bd30d7f6a0fb88c0ef1b0116c3cc09e5e5de17e8
Usama Muhammad <usamamuhammad45678@gmail.com>
2021-10-16 03:58:34 +0500

    half table edit

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -16,6 +16,7 @@ function Table({ id }) {
   const [nextSteps, setNextSteps] = useState("Cattrina Lucas");
   const [admin, setAdmin] = useState(true);
   const [editor, setEditor] = useState(true);
+  const [edit, setEdit]= useState(false)
 
   const updateComment = async (e) => {
     const commentsRef = doc(collection(db, "animalsPost", id, "comments"));
@@ -82,6 +83,31 @@ const unsubscribe = onSnapshot(q, (querySnapshot) => {
   // setComments(tempComments)
   },[])
 
+  const editComment=async (comment, e)=>{
+    setEdit(true)
+    // const q = query(doc(db, "animalsPost", id, "comments", comment.id));
+ 
+    // await setDoc(doc(db, "animalsPost", id, "comments", comment.id), {
+    //    date: date,
+    //    examDetails: examDetails,
+    //    nextSteps: nextSteps,
+    // });
+
+
+  }
+
+  const submitEditedComment= async(comment, e)=>{
+    setEdit(false)
+
+    const q = query(doc(db, "animalsPost", id, "comments", comment.id));
+ 
+    await setDoc(doc(db, "animalsPost", id, "comments", comment.id), {
+       date: date,
+       examDetails: examDetails,
+       nextSteps: nextSteps,
+    });
+  }
+
   return (
     <div className="Table">
       <ReactBootStrap.Table striped bordered hover>
@@ -98,41 +124,54 @@ const unsubscribe = onSnapshot(q, (querySnapshot) => {
               <tr className="center">
                 <td>
                   {admin || editor ? (
-                    <input
-                      type="text"
-                      value={ comment.data.comment.date}
-                      
-                      onChange={(e) => setDate(e.target.value)}
-                    />
+                    edit?<input
+                    type="text"
+                    value={ comment.data?.comment.date}
+                    
+                    onChange={(e) => setDate(e.target.value)}
+                  />:
+                  <h5>{ comment.data?.comment.date}</h5>
                   ) : (
-                    comment.data.comment.date
+                    comment.data?.comment.date
                   )}
+                 
                 </td>
                 <td>
                   {admin || editor ? (
                     <input
                       type="text"
 
-                      value={ comment.data.comment.examDetails}
+                      value={ comment.data?.comment.examDetails}
                       onChange={(e) => setExamDetails(e.target.value)}
                     />
                   ) : (
                     comment.data.comment.examDetails
                   )}
+                  
                 </td>
                 <td>
                   {admin || editor ? (
                     <input
                       type="text"
-                      value={ comment.data.comment.nextSteps}
+                      value={ comment.data?.comment.nextSteps}
                       onChange={(e) => {
                         setNextSteps(e.target.value);
                       }}
                     />
                   ) : (
-                    comment.data.comment.nextSteps
+                    comment.data?.comment.nextSteps
                   )}
                 </td>
+                 {
+                    admin || editor ?
+                    
+                    edit? <button onClick={(e)=> submitEditedComment(comment,e)} >Submit</button>:
+                    <button onClick={(e)=> {
+                   
+                      editComment(comment,e)}} >edit</button>
+                    
+                      :''
+                  }
               </tr>
             );
           })}
